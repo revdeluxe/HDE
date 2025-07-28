2feb3c96961e16067ccf9f915974d3d191d9da71
Richard Villareal <38414584+revdeluxe@users.noreply.github.com>
2025-07-28 11:42:36 +0800

    Test mods

---
+++ backend/RX.py
@@ -0,0 +1,25 @@
+from SX127x.LoRa import LoRa, MODE
+from SX127x.board_config import BOARD
+import time
+
+BOARD.setup()
+BOARD.SpiDev(spi_bus=0, spi_cs=0)
+lora = LoRa(verbose=False, do_calibration=True)
+
+lora.set_mode(MODE.STDBY)
+lora.set_freq(915e6)
+lora.set_spreading_factor(7)
+lora.set_pa_config(pa_select=1, max_power=7, output_power=15)
+lora.set_mode(MODE.RXCONT)
+
+start = time.time()
+while time.time()-start<5:
+    flags = lora.get_irq_flags()
+    if flags.get("rx_done"):
+        raw = bytes(lora.read_payload(nocheck=True))
+        print("Got raw:", raw)
+        break
+    time.sleep(0.01)
+else:
+    print("No RX")
+
---
+++ backend/TX.py
@@ -0,0 +1,20 @@
+from SX127x.LoRa import LoRa, MODE
+from SX127x.board_config import BOARD
+import time
+
+BOARD.setup()
+BOARD.SpiDev(spi_bus=0, spi_cs=0)
+lora = LoRa(verbose=False, do_calibration=True)
+
+lora.set_mode(MODE.STDBY)
+lora.set_freq(915e6)
+lora.set_spreading_factor(7)
+lora.set_pa_config(pa_select=1, max_power=7, output_power=15)
+lora.set_mode(MODE.SLEEP)
+
+# TX loop
+lora.set_mode(MODE.STDBY)
+lora.write_payload([0x01,0x02,0x03,0x04])
+lora.set_mode(MODE.TX)
+time.sleep(1)
+print("Done TX")
