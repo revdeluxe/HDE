33d74bbd0970969ddeb9d4dab548a92a7d14acfb
Richard Villareal <38414584+revdeluxe@users.noreply.github.com>
2025-07-28 13:42:47 +0800

    Update syscheck-lgpio.py

--- backend/syscheck-lgpio.py
+++ backend/syscheck-lgpio.py
@@ -1,23 +1,27 @@
 import lgpio
+import spidev
 import time
 
-CS_PIN = 8
-h = lgpio.gpiochip_open(0)
-lgpio.gpio_claim_output(h, 17, 0)  # RESET = 17
+RESET_PIN = 17  # GPIO 17 (Pin 11)
 
-# Pulse reset
-lgpio.gpio_write(h, 17, 0)
+# ---- Reset LoRa module via GPIO 17 ----
+chip = lgpio.gpiochip_open(0)
+lgpio.gpio_claim_output(chip, RESET_PIN, 0)
+lgpio.gpio_write(chip, RESET_PIN, 0)
 time.sleep(0.1)
-lgpio.gpio_write(h, 17, 1)
+lgpio.gpio_write(chip, RESET_PIN, 1)
 time.sleep(0.1)
+lgpio.gpiochip_close(chip)
 
-def read_register(addr):
-    lgpio.gpio_write(h, CS_PIN, 0)
-    result = lgpio.spi_xfer2(h, [addr & 0x7F, 0x00])
-    lgpio.gpio_write(h, CS_PIN, 1)
-    return result[1]
+# ---- SPI Communication ----
+def read_register(reg_addr):
+    spi = spidev.SpiDev()
+    spi.open(0, 0)  # spidev0.0
+    spi.max_speed_hz = 500000
+    response = spi.xfer2([reg_addr & 0x7F, 0x00])
+    spi.close()
+    return response[1]
 
-print("Reg 0x42 =", hex(read_register(0x42)))
-
-lgpio.gpiochip_close(h)
-
+# ---- Test Register 0x42 ----
+reg_val = read_register(0x42)
+print("Reg 0x42 =", hex(reg_val))
