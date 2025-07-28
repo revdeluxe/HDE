ec26187b75cb9c9c1b349a11d851093592754dfb
Richard Villareal <38414584+revdeluxe@users.noreply.github.com>
2025-07-28 11:44:59 +0800

    try

--- backend/TX.py
+++ backend/TX.py
@@ -4,10 +4,10 @@ import time
 
 BOARD.setup()
 BOARD.SpiDev(spi_bus=0, spi_cs=0)
-lora = LoRa(verbose=False, do_calibration=True)
+lora = LoRa(verbose=False, do_calibration=False)
 
 lora.set_mode(MODE.STDBY)
-lora.set_freq(915e6)
+lora.set_freq(433e6)
 lora.set_spreading_factor(7)
 lora.set_pa_config(pa_select=1, max_power=7, output_power=15)
 lora.set_mode(MODE.SLEEP)
