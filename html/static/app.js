b2077f4abda0607ec139e80e25f2c25303892f9d
Richard Villareal <38414584+revdeluxe@users.noreply.github.com>
2025-07-30 09:39:40 +0800

    Update app.js

--- html/static/app.js
+++ html/static/app.js
@@ -93,8 +93,7 @@ function sentMessage(name) {
 }
 
 function fetchMessages() {
-  checksum = getChecksum();
-  fetch("/api/messages/" + checksum)
+  fetch("/api/messages/")
     .then(response => {
       if (!response.ok) throw new Error("Network response was not ok");
       return response.json();
